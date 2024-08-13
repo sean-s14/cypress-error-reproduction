6717d7c62c710d5c3b98081574c33875ce4fed27
Sean Stocker <sean.stocker14@gmail.com>
2024-08-13 12:58:32 +0100

    feat: Added a beforeEach in cypress test

--- cypress/e2e/start.cy.js
+++ cypress/e2e/start.cy.js
@@ -1,12 +1,15 @@
 describe("Visit home page", () => {
   const BASE_URL = Cypress.env("BASE_URL");
-  it("should display the home page", () => {
+
+  beforeEach(() => {
     cy.visit(BASE_URL);
+  });
+
+  it("should display the home page", () => {
     cy.get("h1").should("contain", "Cypress Error Reproduction");
   });
 
   it("should contain environment variable", () => {
-    cy.visit(BASE_URL);
     cy.get("p").should(
       "contain",
       "Environment Variable: ",
